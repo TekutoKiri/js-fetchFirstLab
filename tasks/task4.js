899e81be86aa1d5f4985fe64740b91f8e30d0e0f
TekutoKiri <kn23-d.soloviov@nubip.edu.ua>
2024-11-17 20:39:15 +0200

    Update task4.js

--- tasks/task4.js
+++ tasks/task4.js
@@ -1,13 +1,19 @@
-"Ваш код повинен зробити DELETE-запит до вказаного URL, де {userId} – це ID користувача, якого потрібно видалити."
-"Поверніть статус відповіді сервера після видалення."
+const fetch = require('node-fetch');
 
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
-
-
-function deleteUser(id) {
-  // Ваш код
+async function deleteUser(id) {
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: 'DELETE'
+    });
+    
+    // Повертаємо статус відповіді сервера після видалення
+    return response.status;
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    return null;
+  }
 }
 
-console.log(deleteUser(1));
+deleteUser(1).then(status => console.log('Response status:', status));
 
 module.exports = deleteUser;
