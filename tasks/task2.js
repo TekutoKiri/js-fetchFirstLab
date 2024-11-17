9c02002aeffd92e539adada5560b179942e8ea45
TekutoKiri <kn23-d.soloviov@nubip.edu.ua>
2024-11-17 20:38:19 +0200

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -1,16 +1,25 @@
-"Ваш код повинен зробити POST-запит до вказаного URL."
-"Для створення нового користувача, передайте в запит наступні дані:"
-"name: ваше ім’я"
-"email: ваш email"
-"Поверніть відповідь від сервера після створення користувача."
+const fetch = require('node-fetch');
 
-"https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
-
-
-function createUser(user) {
-  // Ваш код
+async function createUser(user) {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    });
+    
+    // Повертаємо відповідь від сервера після створення користувача
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error creating user:', error);
+    return null;
+  }
 }
 
-console.log(createUser({name: "Sam", email: "fjsnfkjns2342@gmail.com"}))
+createUser({name: "Sam", email: "fjsnfkjns2342@gmail.com"}).then(response => console.log(response));
 
 module.exports = createUser;
+
